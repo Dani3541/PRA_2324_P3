bf77630e6f753f1c83ac915c369752802fa499cd
Dani <dgonfue@upv.edu.es>
2025-12-11 20:10:39 +0100

    Añadida implementación de la clase TableEntry

---
+++ TableEntry.h
@@ -0,0 +1,32 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V>
+class TableEntry {
+	public:
+		std::string key;
+	       	V value;
+
+		TableEntry() : key(""), value(V()) {}
+		TableEntry(const std::string& k) : key(k), value(V()) {}
+		TableEntry(const std::string& k, V v) : key(k), value(v) {}
+	
+		friend bool operator==(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+			        return te1.key == te2.key;
+		    }
+
+		friend bool operator!=(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+		            return te1.key != te2.key;
+		        }
+
+		friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
+		        out << "(" << te.key << " -> " << te.value << ")";
+		        return out;
+		    }
+};
+
+#endif 
+
